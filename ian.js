034471d368a60f0128caac39842d7fe8775b6400
ian <cemenrttu@gmail.com>
2025-06-29 13:25:55 +0300

    firstcommit

---
+++ ian.js
@@ -0,0 +1,15 @@
+const ian = "Kenyaga"
+
+console.log(ian)
+
+
+const List  = {name : "Iankenyaga", age : 23, School : "Gomycode"}
+
+for(const name in List){
+    console.log(name)
+}
+
+typeof{firstName : "John", lastName : "Doe", age : 30};
+typeof["apples","orange","bananas"]
+typeof null;
+typeof function myFunction(){}
